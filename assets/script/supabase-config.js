49dd963f66fd8bb41b96900bd8a92193990b7d99
rbaprodiscovery-web <rbaprodiscovery@gmail.com>
2026-03-03 00:25:11 -0400

    Export update from ReFramer

--- assets/script/supabase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-export const supabase = createClient(
-  'https://ncasmkneecuaqvzlhfcp.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jYXNta25lZWN1YXF2emxoZmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNjgzNjYsImV4cCI6MjA3MTc0NDM2Nn0.Jt9zKvJ8Zx1fKYaETJolyVwXfQUg3Afnr4TFcWWLM7U'
-);
